de795043f80e975c5db6362752c612f75cca6201
Yash Thakkar <yashthakkar911@gmail.com>
2024-12-17 20:17:12 -0500

    create api interface

---
+++ app/src/main/java/com/example/weatherforecastapp/network/WeatherApi.kt
@@ -0,0 +1,17 @@
+package com.example.weatherforecastapp.network
+
+import com.example.weatherforecastapp.model.Weather
+import com.example.weatherforecastapp.utils.Constants.API_KEY
+import retrofit2.http.GET
+import retrofit2.http.Query
+import javax.inject.Singleton
+
+@Singleton
+interface WeatherApi {
+    @GET(value = "data/2.5/forecast/daily")
+    suspend fun getWeather(
+        @Query("q") query : String,
+        @Query("units") units: String = "imperial",
+        @Query("appid") appid: String = API_KEY // your api key
+    ): Weather
+}
